20c6cb8bc0b4e823abb66048388b243933986cd7
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-19 23:10:31 +0530

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,3 +1,7 @@
-document.getElementById("name").value = "<b>Hello</b>"
-document.forms[0].action="/settings";
-document.forms[0].submit();
+
+window.addEventListener('load', function () {
+  
+    document.getElementById("name").value = "<b>Hello</b>";
+    document.forms[0].action="/settings";
+    document.forms[0].submit();
+   })
